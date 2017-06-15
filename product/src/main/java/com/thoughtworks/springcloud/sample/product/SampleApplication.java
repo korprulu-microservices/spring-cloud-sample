322134f6f3d43dd4dbc88a0b8ce20c5d20099dd5
yeyuexia <yyxworld@gmail.com>
2017-06-15 15:57:28 +0800

    set product connect eureka

--- product/src/main/java/com/thoughtworks/springcloud/sample/product/SampleApplication.java
+++ product/src/main/java/com/thoughtworks/springcloud/sample/product/SampleApplication.java
@@ -2,6 +2,7 @@ package com.thoughtworks.springcloud.sample.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,7 @@ import java.util.List;
 
 @RestController
 @SpringBootApplication
+@EnableEurekaClient
 public class SampleApplication {
 
 	@GetMapping(path = "categories")
